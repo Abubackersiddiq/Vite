98d803ff84c498dfcc08427eff05d65e2f387bf2
Abubackersiddiq <siddiq.a@mitrahsoft.com>
2024-04-24 17:00:43 +0530

    Delopment build

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR devlopment build
         </p>
       </div>
       <p className="read-the-docs">
